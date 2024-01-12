9735181cc11d89f25df3dc9dc8387b85f88ecc34
Chris Jaber <chris.jaber@junkbrands.com>
2024-01-12 16:34:55 -0600

    rechart setup

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,32 @@
+import ChartBox from '../../components/chartBox/ChartBox'
+import TopBox from '../../components/topBox/TopBox'
 import './home.scss'
+import { chartBoxUser,chartBoxProduct,chartBoxRevenue,chartBoxConversion } from '../../data'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4"></div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">Box7</div>
+      <div className="box box8">Box8</div>
+      <div className="box box9">Box9</div>
+    </div>
+  );
 }
 
 export default Home
