31adee8c5ffb9901a074bfb6faff33edd1163cf4
Chris Jaber <chris.jaber@junkbrands.com>
2024-01-12 15:45:01 -0600

    initial setup and build layout components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Dashboard</span>
+      <span>&copy; Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
